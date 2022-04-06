a0c69ad024ad32fd008ea5a1a4803a0b17ceda95
Amber Zimmerman <ajz1221@gmail.com>
2022-04-05 21:23:58 -0600

    setting u[ blog post model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Bp = require('./Bp');
 
-User.hasMany(Project, {
+User.hasMany(Bp, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Bp.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Bp };
